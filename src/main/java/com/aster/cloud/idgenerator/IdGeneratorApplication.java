7137ef2c564a7df60a807a13084811df7f0ebdc4
wangqian <925548289@qq.com>
2021-01-31 19:26:22 +0800

    - 新增单元测试

--- src/main/java/com/aster/cloud/idgenerator/IdGeneratorApplication.java
+++ src/main/java/com/aster/cloud/idgenerator/IdGeneratorApplication.java
@@ -2,11 +2,13 @@ package com.aster.cloud.idgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 王骞
  * @date 2020-10-31
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class IdGeneratorApplication {
     public static void main(String[] args) {
